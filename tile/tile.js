f611161d3a256ce645b718e32cd01c9a399b178c
rs149 <69782915+rs149@users.noreply.github.com>
2020-08-17 14:59:58 +0900

    branchtest

--- tile/tile.js
+++ tile/tile.js
@@ -1,21 +1,30 @@
 window.addEventListener('load', () => {
     var frame = document.getElementById('frame');
-
-    var text = '';
-    var x = 0;
-    var y = 0;
-    var width = 10;
-    for (y = 0; y < width; y++)
-    {
-        for (x = 0; x < width; x++)
-        {
-            var r = Math.floor(Math.random()*256).toString(16);
-            var g = Math.floor(Math.random()*256).toString(16);
-            var b = Math.floor(Math.random()*256).toString(16);
-            var color = '"#' + r + g + b + '"';
-            text += '<font color=' + color + '>■</font>';
+    var count = 0;
+    var id = setInterval(function(){
+        /*
+        count++;
+        if(count > 5){　
+          clearInterval(id);　//idをclearIntervalで指定している
         }
-        text += '<br>';
-    }
-    frame.innerHTML = text;
+        */
+        var text = '';
+        var x = 0;
+        var y = 0;
+        var width = 25;
+        for (y = 0; y < width; y++)
+        {
+            for (x = 0; x < width; x++)
+            {
+                var r = Math.floor(Math.random()*256).toString(16);
+                var g = Math.floor(Math.random()*256).toString(16);
+                var b = Math.floor(Math.random()*256).toString(16);
+                var color = '#' + r + g + b;
+                //text += '<font color=' + color + '>■</font>';
+                text += '<span style="background-color:' + color + ';"> 　 </span>';
+            }
+            text += '<br>';
+        }
+        frame.innerHTML = text;
+    }, 100);
 });
