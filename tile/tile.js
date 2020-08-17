ea0e505f31cad653f2be5d7255d108d9edac3e58
rs149 <69782915+rs149@users.noreply.github.com>
2020-08-17 14:44:47 +0900

    Add files via upload

--- tile/tile.js
+++ tile/tile.js
@@ -1,4 +1,21 @@
 window.addEventListener('load', () => {
     var frame = document.getElementById('frame');
-    frame.innerHTML = 'aaaa';
+
+    var text = '';
+    var x = 0;
+    var y = 0;
+    var width = 10;
+    for (y = 0; y < width; y++)
+    {
+        for (x = 0; x < width; x++)
+        {
+            var r = Math.floor(Math.random()*256).toString(16);
+            var g = Math.floor(Math.random()*256).toString(16);
+            var b = Math.floor(Math.random()*256).toString(16);
+            var color = '"#' + r + g + b + '"';
+            text += '<font color=' + color + '>■</font>';
+        }
+        text += '<br>';
+    }
+    frame.innerHTML = text;
 });
